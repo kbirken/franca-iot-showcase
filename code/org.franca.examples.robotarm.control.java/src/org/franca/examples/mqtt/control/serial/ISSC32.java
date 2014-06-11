16391d90c5b0e9ea669699b038ba2c43d610e851
Tamas Szabo <tamas.szabo@itemis.de>
2014-06-11 12:53:14 +0200

    added required libs/sources

---
+++ code/org.franca.examples.robotarm.control.java/src/org/franca/examples/mqtt/control/serial/ISSC32.java
@@ -0,0 +1,12 @@
+package org.franca.examples.mqtt.control.serial;
+
+public interface ISSC32 {
+
+	void shutdown();
+	boolean reset();
+	void delay(int millisec);
+	boolean move (int ch0, int ch1, int ch2, int ch3, int ch5, int t);
+	
+	/** Control the gripper. */
+	boolean grip (int i);
+}
